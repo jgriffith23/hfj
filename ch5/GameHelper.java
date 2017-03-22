8b037d525e1b22f6658d739117e93566efbb24b3
Jennifer <jennifer.gd42@gmail.com>
2017-03-22 12:56:38 -0700

    Added GameHelper class HFJ said tocopy for now.

---
+++ ch5/GameHelper.java
@@ -0,0 +1,24 @@
+mport java.io.*;
+
+public class GameHelper {
+    public String getUserInput(String prompt) {
+        System.out.print(prompt + " ");
+
+        // Ooh, they're giving us a try/catch. But not explaining yet. :(
+
+        try {
+            BufferedReader is = new BufferedReader(
+                new InputStreamReader(System.in));
+            inputLine = is.readLine();
+            if (inputLine.length() == 0) {
+                return null;
+            }
+        }
+
+        catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+
+        retun inputLine;
+    }
+}
